1f87fb621c8f1b61e914658511a942fa5602e6df
Jerliyah <jerliyahcraig@gmail.com>
2018-02-02 16:28:54 -0600

    corner hugging highlighter, relevant content, and links to articles. Decent first draft

--- action.js
+++ action.js
@@ -2,10 +2,6 @@
 const highlighter = document.querySelector('span#highlighter');
 const hl_items = Array.from( document.querySelectorAll('a') );
 
-const test = document.querySelector('#test');
-
-
-
 
 
 /* ====== Variables ====== */
@@ -25,6 +21,29 @@ function highlight_me(e) {
                                  height: ${my.height + (padding * 2)}px;`
 }
 
+function back_to_station(e) {
+    position = e.target.getClientRects()[0];
+
+    let top = position.y > (window.innerHeight / 2) ? window.innerHeight - 30 :  0;
+    let left = position.x > (window.innerWidth / 2) ?  window.innerWidth - 36 :  0;
+
+    let b1 = top <= 0 && left <= 0 ?  "0" :  "1em";
+    let b2 = top <= 0 && left > 0 ?  "0" :  "1em"; 
+    let b3 = top > 0 && left > 0 ?  "0":  "1em";
+    let b4 = top > 0 && left <= 0 ?  "0" :  "1em";
+
+    highlighter.style.cssText = `top: ${top}px;
+                                 left: ${left}px;
+                                 border-radius: ${b1} ${b2} ${b3} ${b4}; 
+
+                                 width: 30px;
+                                 height: 30px;`
+}
+
+
 
 /* ====== Events ====== */
-hl_items.forEach( (item) => item.addEventListener('mouseover', highlight_me))
+hl_items.forEach( (item) => item.addEventListener('mouseenter', highlight_me))
+hl_items.forEach( (item) => item.addEventListener('mouseleave', back_to_station))
+
+// hl_items.forEach( (item) => item.style.)
